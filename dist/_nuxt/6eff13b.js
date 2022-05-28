c89e45e2c96f86dd18d1d5719a8f31cce4485366
Makinde1034 <makinde1034@gmail.com>
2022-05-28 20:27:49 +0100

    generate dist folder

---
+++ dist/_nuxt/6eff13b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{281:function(e,n,t){"use strict";t.r(n);var l={},c=t(60),component=Object(c.a)(l,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("p",[e._v("Hello i'm the best")])}),[],!1,null,null,null);n.default=component.exports}}]);
